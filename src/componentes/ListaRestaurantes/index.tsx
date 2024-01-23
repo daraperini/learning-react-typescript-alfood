145ca03af0173e63c65f45501884d7a621dbd396
Dara Lahm Perini <daraperini@gmail.com>
2024-01-23 12:47:47 -0300

    Refactoring with API REST and creating admin area

--- src/componentes/ListaRestaurantes/index.tsx
+++ src/componentes/ListaRestaurantes/index.tsx
@@ -1,98 +1,115 @@
-import IRestaurante from '../../interfaces/IRestaurante';
-import style from './ListaRestaurantes.module.scss';
-import Restaurante from './Restaurante';
+import { useEffect, useState } from "react";
+import IRestaurante from "../../interfaces/IRestaurante";
+import style from "./ListaRestaurantes.module.scss";
+import Restaurante from "./Restaurante";
+import axios, { AxiosRequestConfig } from "axios";
+import { IPaginacao } from "../../interfaces/IPaginanacao";
+import {
+  Box,
+  Button,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  TextField,
+} from "@mui/material";
 
-const ListaRestaurantes = () => {
-
-  const restaurantes: IRestaurante[] = [
-    {
-      id: 1,
-      nome: "Lyllys Cafe",
-      pratos: [
-        {
-          id: 1,
-          descricao: 'Lasanha à Bolonhesa',
-          imagem: 'https://receitassaborosa.com/wp-content/uploads/2019/12/Lasanha-com-Molho-a-Bolonhesa.jpg',
-          nome: 'Lasanha',
-          restaurante: 1,
-          tag: 'Italiana'
-        },
-        {
-          id: 2,
-          descricao: 'Strogonoff de Frango à brasileira',
-          imagem: 'https://img.itdg.com.br/images/recipes/000/002/462/332854/332854_original.jpg',
-          nome: 'Strogonoff',
-          restaurante: 1,
-          tag: 'Russa'
-        },
-        {
-          id: 3,
-          descricao: 'Empadão de Frango',
-          imagem: 'https://t1.uc.ltmcdn.com/pt/images/5/7/1/img_como_fazer_empadao_de_frango_27175_600.jpg',
-          nome: 'Empadão de Frango',
-          restaurante: 1,
-          tag: 'Portuguesa'
-        }
-      ]
-    },
-    {
-      id: 2,
-      nome: "Sugiro Sushi",
-      pratos: [
-        {
-          id: 1,
-          descricao: 'Combinado de 8 peças',
-          imagem: 'https://www.sabornamesa.com.br/media/k2/items/cache/5031e263a4a258791d6306b2d3d9dbf6_XL.jpg',
-          nome: 'Sushi',
-          restaurante: 1,
-          tag: 'Japonesa'
-        },
-        {
-          id: 2,
-          descricao: 'Sashimi de Salmão',
-          imagem: 'https://www.comidaereceitas.com.br/img/sizeswp/1200x675/2009/04/sashimi_facil.jpg',
-          nome: 'Sashimi',
-          restaurante: 1,
-          tag: 'Japonesa'
-        }
-      ]
-    },
-    {
-      id: 3,
-      nome: "Cantina da Escola",
-      pratos: [
-        {
-          id: 1,
-          descricao: 'Salgado de queijo com presunto',
-          imagem: 'https://img.itdg.com.br/tdg/images/recipes/000/102/312/279767/279767_original.jpg',
-          nome: 'Quejunto',
-          restaurante: 1,
-          tag: 'Lanche'
-        },
-        {
-          id: 2,
-          descricao: 'Coxinha de Frango',
-          imagem: 'https://t1.rg.ltmcdn.com/pt/posts/1/9/1/coxinha_simples_191_600.jpg',
-          nome: 'Coxinha',
-          restaurante: 1,
-          tag: 'Lanche'
-        },
-        {
-          id: 3,
-          descricao: 'Risole de Palmito',
-          imagem: 'https://img.itdg.com.br/tdg/images/recipes/000/005/116/323871/323871_original.jpg',
-          nome: 'Risole',
-          restaurante: 1,
-          tag: 'Lanche'
-        }
-      ]
-    }
-  ]
-
-  return (<section className={style.ListaRestaurantes}>
-    <h1>Os restaurantes mais <em>bacanas</em>!</h1>
-    {restaurantes?.map(item => <Restaurante restaurante={item} key={item.id} />)}
-  </section>)
+interface IParametrosBusca {
+  ordering?: string;
+  search?: string;
 }
 
-export default ListaRestaurantes
+const ListaRestaurantes = () => {
+  const [restaurantes, setRestaurantes] = useState<IRestaurante[]>([]);
+  const [proximaPagina, setProximaPagina] = useState("");
+  const [paginaAnterior, setPaginaAnterior] = useState("");
+
+  const [busca, setBusca] = useState("");
+  const [ordenador, setOrdenador] = useState("");
+
+  useEffect(() => {
+    carregarDados("http://localhost:8000/api/v1/restaurantes/");
+  }, []);
+
+  const carregarDados = (url: string, opcoes: AxiosRequestConfig = {}) => {
+    axios
+      .get<IPaginacao<IRestaurante>>(url, opcoes)
+      .then((resposta) => {
+        setRestaurantes(resposta.data.results);
+        setProximaPagina(resposta.data.next);
+        setPaginaAnterior(resposta.data.previous);
+      })
+      .catch((erro) => console.log(erro));
+  };
+
+  const buscaRestaurante = (evento: React.FormEvent<HTMLFormElement>) => {
+    evento.preventDefault();
+
+    const opcoes = {
+      params: {} as IParametrosBusca,
+    };
+
+    if (busca) {
+      opcoes.params.search = busca;
+    }
+
+    if (ordenador) {
+      opcoes.params.ordering = ordenador;
+    }
+
+    carregarDados("http://localhost:8000/api/v1/restaurantes/", opcoes);
+  };
+
+  return (
+    <section className={style.ListaRestaurantes}>
+      <h1>
+        Os restaurantes mais <em>bacanas</em>!
+      </h1>
+      <Box
+        sx={{ display: "flex", justifyContent: "center", gap: 5 }}
+        component="form"
+        onSubmit={buscaRestaurante}
+      >
+        <TextField
+          label="Pesquisar Restaurante"
+          variant="outlined"
+          onChange={(evento) => setBusca(evento.target.value)}
+        />
+        <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+          <InputLabel id="select-label">Ordenar</InputLabel>
+          <Select
+            labelId="select-label"
+            value={ordenador}
+            label="Ordenar"
+            onChange={(evento) => setOrdenador(evento.target.value)}
+          >
+            <MenuItem value="nome">Nome</MenuItem>
+            <MenuItem value="id">Id</MenuItem>
+          </Select>
+        </FormControl>
+        <Button type="submit">Buscar</Button>
+      </Box>
+      {restaurantes?.map((item) => (
+        <Restaurante restaurante={item} key={item.id} />
+      ))}
+      {
+        <button
+          onClick={() => carregarDados(paginaAnterior)}
+          disabled={!paginaAnterior}
+        >
+          Página anterior
+        </button>
+      }
+      {
+        <button
+          onClick={() => carregarDados(proximaPagina)}
+          disabled={!proximaPagina}
+        >
+          Próxima página
+        </button>
+      }
+    </section>
+  );
+};
+
+export default ListaRestaurantes;
